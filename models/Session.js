d0f3f4503b71882fd6f386df02f1faf58d6a04a3
Christian Schulze <christian.schulze@mni.thm.de>
2014-05-27 21:29:13 +0200

    added interface to mongoDB with listSessions and addSession

---
+++ models/Session.js
@@ -0,0 +1,43 @@
+function addSession(start_time, end_time, callback) {
+	var MongoClient = require('mongodb').MongoClient;
+
+	MongoClient.connect("mongodb://127.0.0.1:27017/get-productive", function(err, db) {
+        if (err) {
+            callback(err);
+            return;
+
+        } else {
+            db.collection('sessions').insert({
+            	start: start_time,
+            	end: end_time
+            }, function(err, records) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+            });
+        }
+    });
+}
+
+function listSessions(callback) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect("mongodb://127.0.0.1:27017/get-productive", function(err, db) {
+        if (err) {
+            throw err;
+
+        } else {
+            db.collection('sessions').find().toArray(function(err, items) {
+                if (err) {
+                    throw err;
+                } else {
+                    callback(items);
+                }
+            });
+        }
+    });
+}
+
+exports.addSession = addSession;
+exports.listSessions = listSessions;
