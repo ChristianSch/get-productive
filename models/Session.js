4874e1fc7e912021068bc74afff4595a09fe0ccb
Christian Schulze <christian.schulze@mni.thm.de>
2014-05-28 09:25:04 +0200

    formatting improved

--- models/Session.js
+++ models/Session.js
@@ -1,15 +1,15 @@
 function addSession(start_time, end_time, callback) {
-	var MongoClient = require('mongodb').MongoClient;
+    var MongoClient = require('mongodb').MongoClient;
 
-	MongoClient.connect("mongodb://127.0.0.1:27017/get-productive", function(err, db) {
+    MongoClient.connect("mongodb://127.0.0.1:27017/get-productive", function(err, db) {
         if (err) {
             callback(err);
             return;
 
         } else {
             db.collection('sessions').insert({
-            	start: start_time,
-            	end: end_time
+                start: start_time,
+                end: end_time
             }, function(err, records) {
                 if (err) {
                     callback(err);
