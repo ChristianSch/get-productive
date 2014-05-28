73a162dc043cea656d9f8d213c64ec25ab9a10ca
Christian Schulze <christian.schulze@mni.thm.de>
2014-05-28 14:24:27 +0200

    improved order of methods; first meta-methods

--- public/js/lib/time.js
+++ public/js/lib/time.js
@@ -1,25 +1,3 @@
-function minuteDiffWithStamps(stamp1, stamp2) {
-    var date1 = new Date(normalizeTimestamp(stamp1));
-    var date2 = new Date(normalizeTimestamp(stamp2));
-
-    console.log(date1);
-    console.log(date2);
-
-    return Math.abs(date1.getMinutes() - date2.getMinutes());
-}
-
-function hoursAndMinutesWithStamp(stamp) {
-    var date = new Date(stamp);
-
-    return date.getHours() + ":" + normalizeMinutes(date.getMinutes());
-}
-
-function dateWithStamp(stamp) {
-    var date = new Date(stamp);
-
-    return date.getDay() + "." + date.getMonth() + "." + date.getFullYear();
-}
-
 function normalizeMinutes(mins) {
     if (mins < 10) {
         return '0' + mins;
@@ -48,6 +26,25 @@ function isValidDate(d) {
         return false;
     }
     return !isNaN(d.getTime());
+}
+
+function minuteDiffWithStamps(stamp1, stamp2) {
+    var date1 = new Date(normalizeTimestamp(stamp1));
+    var date2 = new Date(normalizeTimestamp(stamp2));
+    
+    return Math.abs(parseInt(date1.getMinutes() - date2.getMinutes()));
+}
+
+function hoursAndMinutesWithStamp(stamp) {
+    var date = new Date(stamp);
+
+    return date.getHours() + ":" + normalizeMinutes(date.getMinutes());
+}
+
+function dateWithStamp(stamp) {
+    var date = new Date(stamp);
+
+    return date.getDay() + "." + date.getMonth() + "." + date.getFullYear();
 }
 
 exports.minuteDiffWithStamps = minuteDiffWithStamps;
