b8ed0bb244b3fccffd349cb5e270eec57362c8cb
Christian Schulze <christian.schulze@mni.thm.de>
2014-06-15 20:24:58 +0200

    api route externalised

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express'),
     exphbs = require('express3-handlebars'),
-    path = require('path'),
-    timeLib = require(__dirname + '/public/js/lib/time.js');
-
-var Session = require(__dirname + '/models/Session.js')(process.env.MONGODB_URI, "get-productive");
+    path = require('path');
 
 var app = exports.app = express();
 
@@ -32,42 +29,10 @@ app.get('/', function(req, res) {
         title: 'Get Productive!'
     });
 });
-/* >>>>> API <<<<< */
-app.post('/api/add_session', function(req, res) {
-    var start = req.param('start');
-    var end = req.param('end');
 
-    if (timeLib.isValidDate(new Date(parseInt(start))) &&
-        timeLib.isValidDate(new Date(parseInt(end)))) {
-        Session.addSession(start, end, function(err) {
-            if (err) {
-                console.log(err);
-                res.statusCode = 500;
-
-            } else {
-                res.statusCode = 200;
-                res.send(req.body);
-            }
-        });
-
-    } else {
-        res.statusCode = 400;
-        res.send("Invalid time");
-    }
-});
-
-app.get('/api/list_sessions', function(req, res) {
-    Session.listSessions(function(err, items) {
-        if (err) {
-            res.statusCode = 500;
-            console.log(err.message);
-
-        } else {
-            res.statusCode = 200;
-            res.send(items);
-        }
-    });
-});
+/* api */
+var api = require('./lib/api')
+app.use(api);
 
 /* >>>>> error handling <<<<< */
 // see: https://github.com/visionmedia/express/blob/master/examples/error-pages/index.js
@@ -138,6 +103,5 @@ app.use(function(err, req, res, next) {
         error_num: 500
     });
 });
-
 
 app.listen(7600);
