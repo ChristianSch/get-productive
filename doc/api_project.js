a1e2326fde2aabffff445715b879b807cd36a05f
Christian Schulze <christian.schulze@mni.thm.de>
2014-06-18 00:38:37 +0200

    API documentation for `User` added with apidoc

---
+++ doc/api_project.js
@@ -0,0 +1,10 @@
+define({
+  "name": "get-productive",
+  "version": "0.0.1",
+  "description": "find your most productive time of the day",
+  "apidoc": "",
+  "generator": {
+    "version": "0.4.4",
+    "time": "2014-06-17T22:37:39.475Z"
+  }
+});
