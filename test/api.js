5b1358b0bff3d950bd23cb18eab7eee7d74d6e27
Christian Schulze <christian.schulze@mni.thm.de>
2016-05-29 12:02:50 +0200

    most recent api changes

--- test/api.js
+++ test/api.js
@@ -94,6 +94,7 @@ describe('GET /api/v1/user', function() {
                 done();
             });
     });
+
     it('should return status 404', function(done) {
         request(app)
             .get('/api/v1/user/000000000000000000000000')
@@ -103,6 +104,7 @@ describe('GET /api/v1/user', function() {
                 done();
             });
     });
+
     it('should return status 200', function(done) {
         request(app)
             .get('/api/v1/user/' + testUser.id)
@@ -116,9 +118,85 @@ describe('GET /api/v1/user', function() {
 });
 
 describe('PUT /api/v1/user', function() {
+    it('should return status 422', function(done) {
+        request(app)
+            .put('/api/v1/user/thisisnoid')
+            .expect(422)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
 
+    it('should return status 404', function(done) {
+        request(app)
+            .put('/api/v1/user/000000000000000000000000')
+            .expect(404)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+
+    it('should return 200 and should\'ve changed only the name', function(done) {
+        testUser.name = "barfoo";
+        request(app)
+            .put('/api/v1/user/' + testUser.id)
+            .expect(200)
+            .send({
+                "name": testUser.name
+            })
+            .end(function(err, res) {
+                if (err) return done(err);
+
+                var doc = res.body;
+
+                assert.equal(doc.local.name, testUser.name, "name should've been changed");
+                assert.equal(doc.local.mail, testUser.mail, "mail should remain unchanged");
+
+                done();
+            });
+    });
+    it('should return 200 and should\'ve changed only the mail', function(done) {
+        testUser.mail = "bar@foo.xl";
+        request(app)
+            .put('/api/v1/user/' + testUser.id)
+            .expect(200)
+            .send({
+                "mail": testUser.mail
+            })
+            .end(function(err, res) {
+                if (err) return done(err);
+
+                var doc = res.body;
+                testUser.id = doc._id;
+
+                assert.equal(doc.local.name, testUser.name, "name should remain unchanged");
+                assert.equal(doc.local.mail, testUser.mail, "mail should've been changed");
+
+                done();
+            });
+    });
 });
 
 describe('DELETE /api/v1/user', function() {
+    it('should return status 422', function(done) {
+        request(app)
+            .delete('/api/v1/user/thisisnoid')
+            .expect(422)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
 
+    it('should return status 404', function(done) {
+        request(app)
+            .delete('/api/v1/user/000000000000000000000000')
+            .expect(404)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
 });
